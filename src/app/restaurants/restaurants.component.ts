b463f003ae0134623b8e630778b467a4fda3dc49
marcuspss <mpss.frb@gmail.com>
2019-08-02 17:27:28 -0300

    commit trabalho

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -9,12 +9,12 @@ import { RestaurantsService } from './restaurants.service';
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[] 
+  restaurants: Restaurant[]
 
   constructor(private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
-    this.restaurants = this.restaurantsService.restaurants()
+    this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants)
   }
 
 }
