a2ec34ce52481ec0c3609cbc7583b2a115a59fa4
marcuspss <mpss.frb@gmail.com>
2019-08-09 14:16:16 -0300

    comit trabalho

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -28,4 +28,10 @@ export class RestaurantsService {
         .map(response => response.json())
         .catch(ErrorHandler.handlerError)
       }
+
+      reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handlerError)
+      }
 }
