9239880de106b418a80d0b88cdb41354f4911cfa
marcuspss <mpss.frb@gmail.com>
2019-08-27 16:50:21 -0300

    Trabalho

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service';
+import { MenuItem } from '../menu-item/menu-item.model';
 
 @Component({
   selector: 'mt-shopping-cart',
